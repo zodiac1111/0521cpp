58900a9f9ed66b16244d483ee92df89d6f4382c9
zodiac1111 <zodiac8765+git@gmail.com>
2012-05-21 17:33:20 +0800

    代码缩进

--- fruit.h
+++ fruit.h
@@ -3,26 +3,25 @@
 #include<iostream>
 using namespace std;
 
-class c1
-{
-	public:
-		c1(void);//构造函数reload
-		c1(int i);
-		~c1(void);
+class c1 {
+ public:
+	c1(void);		//构造函数reload
+	 c1(int i);
+	~c1(void);
 
-	virtual void print(void);//虚函数
+	virtual void print(void);	//虚函数
 	void set(void);
 	int i;
 };
-class c2:public c1//继承
+class c2:public c1		//继承
 {
-	public:
-	c2(int i,int m2);//构造函数reload
-	c2(void);
+ public:
+	c2(int i, int m2);	//构造函数reload
+	 c2(void);
 	~c2(void);
 	//int i;
 	int m2;
-	virtual void print(void);//虚函数
+	virtual void print(void);	//虚函数
 };
 /*
 void funp(c1 &o)
